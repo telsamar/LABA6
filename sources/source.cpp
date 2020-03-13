874de633fa5eb5c834c76773b6b32c7fbdb84eaf
telsamar <Stolya2000@mail.ru>
2020-03-13 13:41:24 +0300

    chn4

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,20 +1,20 @@
 // Copyright 2020 <telsamar>
-
 #include <header.hpp>
 
 int main(int argc, char* argv[]) {
-  size_t M = 0;
-  if (argc > 1) M = atoi(argv[1]);
-  if (M == 0) M = thread::hardware_concurrency();
-  boost::log::register_simple_formatter_factory<
-      boost::log::trivial::severity_level, char>("Severity");
-  init_logs();
-  boost::log::add_common_attributes();
-  BOOST_LOG_TRIVIAL(trace) << "Threads amount: " << M;
-  vector<thread*> threads;
-  threads.resize(M);
-  for (size_t i = 0; i < M; i++) threads[i] = new thread(logs);
-  for (auto th : threads) th->join();
-  return 0;
+	size_t M = thread::hardware_concurrency();
+	if (argc > 1)
+		M = atoi(argv[1]);
+	boost::log::register_simple_formatter_factory<boost::log::trivial::severity_level, char>("Severity");
+	init_logs();
+	boost::log::add_common_attributes();
+	BOOST_LOG_TRIVIAL(trace) << "Threads amount: " << M;
+	vector<thread*> threads;
+	threads.resize(M);
+	for (size_t i = 0; i < M; i++)
+		threads[i] = new thread(logs);
+	for (auto th : threads)
+		th->join();
+	return 0;
 }
 
