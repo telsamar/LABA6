e7685ab01d3ac9c66703c186e5ef53401c8e48f3
telsamar <Stolya2000@mail.ru>
2020-03-28 11:12:02 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -20,7 +20,7 @@ using std::string;
 using std::size_t;
 using std::to_string;
 using std::rand;
-static const char IDEAL[0000];
+auto IDEAL = 1000;
 
 static const int SIZE_ROTATION(5*1024*1024);
 static const int ZERO(0);
